16f65f045b2ee0065ac9f8dcdcf5023bc37a69d4
sonolilis <soichiro.koike1365@gmail.com>
2021-02-27 19:06:14 +0900

    ユーザー管理

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,28 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_many :items
+  has_many :orders
+  has_many :comments
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  VALID_NAMEKANA_REGEX = /\A[ァ-ヶー－]+\z/
+
+  with_options presence: true, format: { with: VALID_NAME_REGEX, message: 'is invalid. Input full-width characters.' } do
+    validates :first_name
+    validates :last_name
+  end
+  with_options presence: true, format: { with: VALID_NAMEKANA_REGEX, message: 'is invalid. Input full-width katakana characters.' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+  validates :nickname, presence: true
+  validates :password, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }, confirmation: true
+  validates :email, presence: true, uniqueness: true, format: { with:VALID_EMAIL_REGEX, message: 'is invalid. Format is not.' }
+  validates :birthday, presence: true
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    first_name             { '山田' }
+    last_name              { '太郎' }
+    first_name_kana        { 'ヤマダ' }
+    last_name_kana         { 'タロウ' }
+    nickname               { Faker::Name.initials(number: 2) }
+    birthday               { Faker::Date.birthday }
+    email                  { Faker::Internet.free_email }
+    password               { Faker::Internet.password(min_length: 6) }
+    password_confirmation  { password }
+  end
+end

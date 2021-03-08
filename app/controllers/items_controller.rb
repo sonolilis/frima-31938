a7ffa2d8642309d5a0bc8a642552bbb360dbd161
sonolilis <soichiro.koike1365@gmail.com>
2021-03-08 21:34:14 +0900

    不要な箇所のコメントアウト等

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :destroy]
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  #before_action :authenticate_user!, only: [:new, :edit, :destroy, :create]
+  #before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     #@items = Item.all.includes(:user).order("created_at DESC")
@@ -26,8 +26,8 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:product_name, :discription, :category_id, :status_id, :price, :shipping_id, :shipment_source_id, :date_of_shipment_id, :image).merge(user_id: current_user.id)
   end
 
-  def set_item
-    @item = Item.includes(:user).find(params[:id])
-  end
+  #def set_item
+    #@item = Item.includes(:user).find(params[:id])
+  #end
 
 end

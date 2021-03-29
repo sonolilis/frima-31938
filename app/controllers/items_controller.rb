d459b267672652942a0811e68b640bbe1417187f
sonolilis <soichiro.koike1365@gmail.com>
2021-03-29 16:41:24 +0900

    edirect_to_root_path追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
   before_action :item_find_initialize, only: [:show, :edit, :update]
+  before_action :redirect_to_root_path, only [:edit, :update]
 
   def index
     @items = Item.all.includes(:user).order("created_at DESC")
@@ -23,9 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id != @item.user.id
-      redirect_to root_path
-    end
   end
 
   def update
@@ -45,6 +43,12 @@ class ItemsController < ApplicationController
 
   def item_find_initialize
     @item = Item.find(params[:id])
+  end
+
+  def redirect_to_root_path
+    if current_user.id != @item.user.id
+      redirect_to root_path
+    end
   end
 
 end
